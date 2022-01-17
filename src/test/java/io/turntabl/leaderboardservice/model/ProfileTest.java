ef1e6fa18a01ec713983586d395fcc35f7250826
George Hanson <georgehans301@gmail.com>
2022-01-17 08:43:28 +0000

    added profile test

--- src/test/java/io/turntabl/leaderboardservice/model/ProfileTest.java
+++ src/test/java/io/turntabl/leaderboardservice/model/ProfileTest.java
@@ -1,5 +1,19 @@
 package io.turntabl.leaderboardservice.model;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProfileTest {
+
+
+    @Test
+    void profileSetLanguageLevels(){
+        Profile profile = new Profile();
+        LanguageLevel languageLevel = new LanguageLevel();
+        languageLevel.setName("George");
+        languageLevel.setRank(399);
+        assertEquals(profile,profile.setLanguageLevels(List.of(languageLevel)));
+
+    }
 
 }
